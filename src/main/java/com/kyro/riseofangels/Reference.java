3c05475e9e7150cebc1ef068bcdb059a7d056210
a1re1 <twwhitehurst@gmail.com>
2017-03-14 14:47:03 -0400

    src

---
+++ src/main/java/com/kyro/riseofangels/Reference.java
@@ -0,0 +1,7 @@
+package com.kyro.riseofangels;
+
+public class Reference {
+	public static final String MOD_ID = "RoA";
+	public static final String MOD_NAME = "Rise of Angels";
+	public static final String VERSION = "1.0";
+}
---
+++ src/main/java/com/kyro/riseofangels/RoAItems.java
@@ -0,0 +1,22 @@
+package com.kyro.riseofangels;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class RoAItems {
+	
+	public static 
+	
+	public static void init() {
+	}
+	
+	public static void register()
+	{
+		
+	}
+	
+	public static void registerRenders()
+	{
+		
+	}
+	
+}
